c24003c68df28ffe55fc0951ad6a758e15f1ef15
Philipp Boersch-Supan <pboesu@gmail.com>
2026-06-16 14:04:15 +0100

    tidying up of docs and adding compiler flags to ensure build/documentation does not stall because of large compiled objects

--- R/moultmcmcExtra-package.R
+++ R/moultmcmcExtra-package.R
@@ -1,14 +1,16 @@
 #' The 'moultmcmcExtra' package.
 #'
-#' @description A DESCRIPTION OF THE PACKAGE
+#' @description Add-on functionality for moultmcmc
 #'
-#' @docType package
-#' @name moultmcmcExtra-package
+#' @keywords internal
+"_PACKAGE"
 #' @aliases moultmcmcExtra
 #' @useDynLib moultmcmcExtra, .registration = TRUE
 #' @import methods
 #' @import Rcpp
 #' @importFrom rstan sampling
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel RcppParallelLibs
 #'
 #' @references
 #' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.26.11. https://mc-stan.org
